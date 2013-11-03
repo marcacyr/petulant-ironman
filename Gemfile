9de8c61502956ec6f5c42e177ceba9cf3b805cdd
Marc Cyr <marcacyr@gmail.com>
2013-11-03 14:44:23 -0800

    initial commit all files

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'jquery-rails'
+
+
+group :development do
+	gem 'sqlite3'
+	gem 'guard-livereload'
+end
+
+group :production do
+	gem 'pg'
+end
+
+
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'bootstrap-sass', '~> 2.2.2.0'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DrcyrSite::Application.config.secret_token = 'd6c6a069c963d64b8f6005b50726df62cfb883f287c6cb19c9fdd755c00a73132f41d8073fc8b6e94cec4aef146c251643df99481cd5e511a6a1e07bf3dadb99'
